f00884228cf287d44e2be5217094abdd956831ea
Jose Becerra <becerra.pucp@gmail.com>
2024-09-21 17:19:48 -0500

    47. Actualizar la cantidad del producto

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -25,7 +25,18 @@ export class CartAppComponent implements OnInit{
   }
 
   addProdctToCart(product: Product) { 
-    this.items = [... this.items, { product, quantity: 1}]
+    const hasItem = this.items.find(item =>  item.product.id === product.id);
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id) {
+          return {...item, quantity: item.quantity +1}
+        }
+        return item;
+      })
+    } else {
+      this.items = [... this.items, { product, quantity: 1}]
+    }
+    
   }
 
 }
