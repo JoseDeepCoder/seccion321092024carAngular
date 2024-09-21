ee832abb76b0b6a96e7838b9bec2c14df215fb7c
Jose Becerra <becerra.pucp@gmail.com>
2024-09-21 13:23:21 -0500

    chore: first commit

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'cart-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-app.component.html'
+})
+export class CartAppComponent {
+
+  products: Product[]  = [];
+
+  constructor(private service: ProductService) { 
+    
+  }
+
+}
